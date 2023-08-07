a2e8cb9439c545b54db2d2451ff19a8ed075151d
brendanjeffreymcmahon <brendanjeffreymcmahon@gmail.com>
2023-08-07 11:24:41 -0400

    try another config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    base: process.env.NODE_ENV === "production" ? "/mealcraft-ui/" : "/",
+    assetsDir: "assets",
+  },
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/mealcraft-ui/" : "/",
 });
