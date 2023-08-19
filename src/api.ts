c304009ffd1255b16b1a35e19c5b31755ebf8e17
brendanjeffreymcmahon <brendanjeffreymcmahon@gmail.com>
2023-08-19 16:54:07 -0400

    don't allow duplicate tags when saving/updating

--- src/api.ts
+++ src/api.ts
@@ -1,45 +1,54 @@
-import axios, { AxiosResponse } from 'axios';
-import { Ingredient } from './Ingredient';
+import axios, { AxiosResponse } from "axios";
+import { Ingredient } from "./Ingredient";
 
 const API_URL = "https://ytkjy0j5al.execute-api.us-east-2.amazonaws.com/prod";
 
 export function getIngredient(ingredientId: string): Promise<Ingredient> {
-  return axios.get<Ingredient>(`${API_URL}/ingredients?ingredientId=${ingredientId}`)
+  return axios
+    .get<Ingredient>(`${API_URL}/ingredients?ingredientId=${ingredientId}`)
     .then((response: AxiosResponse<Ingredient>) => response.data)
-    .catch(error => {
+    .catch((error) => {
       throw error;
     });
 }
 
 export function getAllIngredients(): Promise<Ingredient[]> {
-  return axios.get<Ingredient[]>(`${API_URL}/ingredients/`)
+  return axios
+    .get<Ingredient[]>(`${API_URL}/ingredients/`)
     .then((response: AxiosResponse<Ingredient[]>) => response.data)
-    .catch(error => {
+    .catch((error) => {
       throw error;
     });
 }
 
 export function updateIngredient(ingredient: Ingredient): Promise<Ingredient> {
-  return axios.put<Ingredient>(`${API_URL}/ingredients`, ingredient)
+  ingredient.tags = [...new Set(ingredient.tags)];
+
+  return axios
+    .put<Ingredient>(`${API_URL}/ingredients`, ingredient)
     .then((response: AxiosResponse<Ingredient>) => response.data)
-    .catch(error => {
+    .catch((error) => {
       throw error;
     });
 }
 
 export function saveIngredient(ingredient: Ingredient): Promise<Ingredient> {
-  return axios.post<Ingredient>(`${API_URL}/ingredients`, ingredient)
+  ingredient.tags = [...new Set(ingredient.tags)];
+
+  return axios
+    .post<Ingredient>(`${API_URL}/ingredients`, ingredient)
     .then((response: AxiosResponse<Ingredient>) => response.data)
-    .catch(error => {
+    .catch((error) => {
       throw error;
     });
 }
 
 export function deleteIngredient(ingredientId: string): Promise<void> {
-  console.log('deleting');
-  return axios.delete<void>(`${API_URL}/ingredients?ingredientId=${ingredientId}`)
+  console.log("deleting");
+  return axios
+    .delete<void>(`${API_URL}/ingredients?ingredientId=${ingredientId}`)
     .then((response: AxiosResponse<void>) => response.data)
-    .catch(error => {
+    .catch((error) => {
       throw error;
     });
 }
