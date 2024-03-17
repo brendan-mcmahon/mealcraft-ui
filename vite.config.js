fa33094e05757ab15fa6854141a1d0302eb116c0
Brendan McMahon <brendanjeffreymcmahon@gmail.com>
2024-03-17 07:24:15 -0400

    Update vite config for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/mealcraft-ui/" : "/",
-
+  build: {
+    outDir: './dist',
+  },
+  server: {
+    port: 3000,
+  },
 });
